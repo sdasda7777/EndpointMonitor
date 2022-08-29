c2a9131b64bb6793f97ed7e5d7594e68dc8e8c9a
sdasda7777 <17746796+sdasda7777@users.noreply.github.com>
2022-08-29 12:09:37 +0200

    Added base class, pom.xml and application.properties

---
+++ src/main/java/com/sdasda7777/endpointmonitor/EndpointMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.sdasda7777.endpointmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointMonitorApplication.class, args);
+    }
+
+}
