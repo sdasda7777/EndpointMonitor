c4e300a5c31d6a9bfdd9211b2d0b1f19d58383e1
sdasda7777 <17746796+sdasda7777@users.noreply.github.com>
2022-08-30 14:57:02 +0200

    Added multithreading to MonitoringService

--- src/main/java/com/sdasda7777/endpointmonitor/L02/MonitoringService.java
+++ src/main/java/com/sdasda7777/endpointmonitor/L02/MonitoringService.java
@@ -3,7 +3,6 @@ package com.sdasda7777.endpointmonitor.L02;
 import com.sdasda7777.endpointmonitor.L02.Entities.MonitoredEndpoint;
 import com.sdasda7777.endpointmonitor.L02.Entities.MonitoringResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,56 +16,77 @@ import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Configuration
 @EnableScheduling
 public class MonitoringService {
+    private class EndpointCheckWorkerThread implements Runnable {
+        private MonitoredEndpoint endpoint;
+
+        public EndpointCheckWorkerThread(MonitoredEndpoint endpoint){
+            this.endpoint = endpoint;
+        }
+
+        @Override
+        public void run() {
+            MonitoringResult status = checkStatus(endpoint.getUrl());
+            status.setMonitoredEndpoint(endpoint);
+            monitoringResultService.createMonitoringResult(status);
+            monitoredEndpointService.updateEndpointLastCheck(endpoint, status.getCheckDate());
+        }
+
+        static MonitoringResult checkStatus(String url){
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("accept", "application/json")
+                    .build();
+
+            LocalDateTime requestTime = LocalDateTime.now();
+            try {
+                HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+                MonitoringResult ret = new MonitoringResult();
+                ret.setCheckDate(requestTime);
+                ret.setResultStatusCode(response.statusCode());
+                ret.setResultPayload(response.body());
+
+                return ret;
+            } catch (ConnectException c) {
+                MonitoringResult ret = new MonitoringResult();
+                ret.setCheckDate(requestTime);
+                ret.setResultStatusCode(599);
+                ret.setResultPayload("Connection to server hosting URL '" + url
+                        + "' (if such server exists) could not be established.");
+                return ret;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     @Autowired
     MonitoredEndpointService monitoredEndpointService;
     @Autowired
     MonitoringResultService monitoringResultService;
 
-    MonitoringResult checkStatus(String url){
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .header("accept", "application/json")
-                .build();
+    ExecutorService executor;
 
-        LocalDateTime requestTime = LocalDateTime.now();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            MonitoringResult ret = new MonitoringResult();
-            ret.setCheckDate(requestTime);
-            ret.setResultStatusCode(response.statusCode());
-            ret.setResultPayload(response.body());
-
-            return ret;
-        } catch (ConnectException c) {
-            MonitoringResult ret = new MonitoringResult();
-            ret.setCheckDate(requestTime);
-            ret.setResultStatusCode(599);
-            ret.setResultPayload("Connection to server hosting URL '" + url
-                                    + "' (if such server exists) could not be established.");
-            return ret;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public MonitoringService(){
+        this.executor = Executors.newFixedThreadPool(
+                            Runtime.getRuntime().availableProcessors());
     }
 
     @Scheduled(fixedDelay = 1000)
     public void scheduleFixedDelayTask() {
         Collection<MonitoredEndpoint> needUpdating = monitoredEndpointService.getRequiringUpdate();
-
-        //TODO multithreading
+        
         for(MonitoredEndpoint endpoint : needUpdating){
-            MonitoringResult status = checkStatus(endpoint.getUrl());
-            status.setMonitoredEndpoint(endpoint);
-            monitoringResultService.createMonitoringResult(status);
-            monitoredEndpointService.updateEndpointLastCheck(endpoint, status.getCheckDate());
+            executor.execute(new EndpointCheckWorkerThread(endpoint));
         }
     }
 }
