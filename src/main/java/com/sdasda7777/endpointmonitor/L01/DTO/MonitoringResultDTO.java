f826f654a244ebc5ce2af5b3be987403caf5cfc0
sdasda7777 <17746796+sdasda7777@users.noreply.github.com>
2023-07-12 20:31:39 +0200

    Added comments to/cleaned up DTOs

--- src/main/java/com/sdasda7777/endpointmonitor/L01/DTO/MonitoringResultDTO.java
+++ src/main/java/com/sdasda7777/endpointmonitor/L01/DTO/MonitoringResultDTO.java
@@ -1,22 +1,36 @@
 package com.sdasda7777.endpointmonitor.L01.DTO;
 
-import com.sdasda7777.endpointmonitor.L02.Entities.MonitoredEndpoint;
 import com.sdasda7777.endpointmonitor.L02.Entities.MonitoringResult;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Objects;
+
+/**
+ * This is a <a href="https://en.wikipedia.org/wiki/Data_transfer_object">DTO</a> version of the {@link com.sdasda7777.endpointmonitor.L02.Entities.MonitoringResult} entity
+ */
 
 public class MonitoringResultDTO {
-    Long id;
 
-    LocalDateTime checkDate;
-    Integer resultStatusCode;
-    String resultPayload;
+    /** ID number of the MonitoringResult entity */
+    private final Long id;
 
-    Long monitoredEndpointId;
+    /** Date when the monitoring was performed */
+    private final LocalDateTime checkDate;
 
+    /** <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes">HTTP status code</a> of the endpoint response */
+    private final Integer resultStatusCode;
+
+    /** Payload (<a href="https://en.wikipedia.org/wiki/HTTP_message_body">HTTP body<a/>) of the endpoint response */
+    private final String resultPayload;
+
+    /** ID of the monitored endpoint */
+    private final Long monitoredEndpointId;
+
+    /**
+     * Constructor for MonitoringResultDTO which automatically takes all relevant data from the passed MonitoringResult entity
+     * @param monitoringResult entity to take relevant data from
+     */
     public MonitoringResultDTO(MonitoringResult monitoringResult){
         this.id = monitoringResult.getId();
         this.checkDate = monitoringResult.getCheckDate();
@@ -25,6 +39,14 @@ public class MonitoringResultDTO {
         this.monitoredEndpointId = monitoringResult.getMonitoredEndpoint().getId();
     }
 
+    /**
+     * Constructor for MonitoringResultDTO which takes relevant data as arguments
+     * @param id id of the endpoint entity
+     * @param checkDate date when the monitoring was performed
+     * @param resultStatusCode <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes">HTTP status code</a> of the endpoint response
+     * @param resultPayload payload (<a href="https://en.wikipedia.org/wiki/HTTP_message_body">HTTP body<a/>) of the endpoint response
+     * @param monitoredEndpointId ID of the monitored endpoint entity
+     */
     public MonitoringResultDTO(Long id, LocalDateTime checkDate,
                                Integer resultStatusCode, String resultPayload,
                                Long monitoredEndpointId){
@@ -35,43 +57,81 @@ public class MonitoringResultDTO {
         this.monitoredEndpointId = monitoredEndpointId;
     }
 
+    /**
+     * ID getter
+     * @return ID of the monitoring result
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     * Check date getter
+     * @return date when the monitoring was performed
+     */
     public LocalDateTime getCheckDate() {
         return checkDate;
     }
 
+    /**
+     * Response status code getter
+     * @return <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes">HTTP status code</a> of the endpoint response
+     */
     public Integer getResultStatusCode() {
         return resultStatusCode;
     }
 
+    /**
+     * Response payload getter
+     * @return payload (<a href="https://en.wikipedia.org/wiki/HTTP_message_body">HTTP body<a/>) of the endpoint response
+     */
     public String getResultPayload() {
         return resultPayload;
     }
 
+    /**
+     * Endpoint entity ID
+     * @return ID of the endpoint entity which this result belongs to
+     */
     public Long getMonitoredEndpointId() {
         return monitoredEndpointId;
     }
 
 
+    /**
+     * equals - determines whether two objects are equivalent
+     * @param o compared object
+     * @return true iff values match for every field
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MonitoringResultDTO that = (MonitoringResultDTO) o;
-        return Objects.equals(id, that.id) && Objects.equals(checkDate, that.checkDate) && Objects.equals(resultStatusCode, that.resultStatusCode) && Objects.equals(resultPayload, that.resultPayload) && Objects.equals(monitoredEndpointId, that.monitoredEndpointId);
+        return id.equals(that.id)
+                && checkDate.equals(that.checkDate)
+                && resultStatusCode.equals(that.resultStatusCode)
+                && resultPayload.equals(that.resultPayload)
+                && monitoredEndpointId.equals(that.monitoredEndpointId);
     }
 
-
-    public static MonitoringResultDTO convertOne(MonitoringResult endpoint){
-        return new MonitoringResultDTO(endpoint);
+    /**
+     * Converts one MonitoringResult to one MonitoringResultDTO
+     * @param result source result entity
+     * @return MonitoringResultDTO describing provided result entity
+     */
+    public static MonitoringResultDTO convertOne(MonitoringResult result){
+        return new MonitoringResultDTO(result);
     }
 
-    public static Collection<MonitoringResultDTO> convertMany(Collection<MonitoringResult> endpoints){
+    /**
+     * Converts collection of MonitoringResults to collection of MonitoringResultDTOs
+     * @param results collection of results entities
+     * @return collection of MonitoringResultDTOs describing provided result entities
+     */
+    public static Collection<MonitoringResultDTO> convertMany(Collection<MonitoringResult> results){
         Collection<MonitoringResultDTO> dtos = new ArrayList<>();
-        endpoints.forEach(i -> dtos.add(MonitoringResultDTO.convertOne(i)));
+        results.forEach(ii -> dtos.add(MonitoringResultDTO.convertOne(ii)));
         return dtos;
     }
 }
